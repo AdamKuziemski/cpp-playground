504e600d5473b4e32f83d4a480ece11ce2790656
Adam Kuziemski <kuziemski.a@gmail.com>
2016-11-23 10:33:31 +0000

    Add files via upload

---
+++ logs/main.cpp
@@ -0,0 +1,16 @@
+#include "logger.hpp"
+
+int main()
+{
+  Logger log;
+  std::map<std::string, std::string> testMap {{"Hello", "Dumps"}, {"Just", "Testing"}, {"Some longer name", "Longer value"}};
+
+  log(ErrorEvent("Test error"));
+  log(InfoEvent("Info test"));
+  log(SuccessEvent("Success test"));
+  log("Normal event");
+  log.separator();
+  log(DumpEvent(testMap));
+
+  return 0;
+}
