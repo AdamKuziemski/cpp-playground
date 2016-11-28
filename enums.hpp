35ef9191bf943a84a7cb0ae28b8c507bb560f375
Adam Kuziemski <kuziemski.a@gmail.com>
2016-11-28 15:52:20 +0000

    Update enums.hpp

--- enums.hpp
+++ enums.hpp
@@ -36,7 +36,7 @@ struct EnumValues
 };
 
 template <typename E>
-constexpr auto to_integral(E e) -> typename std::underlying_type<E>::type
+constexpr auto to_underlying(E e) -> typename std::underlying_type<E>::type
 {
   return static_cast<typename std::underlying_type<E>::type>(e);
 }
