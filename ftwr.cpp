a1b6534af633b949ded63bd10ef51660aa8bd5fc
Adam Kuziemski <kuziemski.a@gmail.com>
2016-11-29 05:54:00 +0000

    Add files via upload

---
+++ ftwr.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <random>
+using namespace std;
+
+void typeText(string text, int showFor = 0, bool clear = true)
+{
+  static mt19937 mt(chrono::system_clock::now().time_since_epoch().count());
+
+  for(char c : text)
+  {
+    cout << c;
+    // sleep for random times to achieve the effect of typing
+    this_thread::sleep_for(chrono::milliseconds(40 + (mt() % 460)));
+  }
+
+  if(showFor > 0)
+    this_thread::sleep_for(chrono::milliseconds(showFor));
+
+  if(clear)
+    cout <<'\r'<< string(text.size(), " ") <<'\r';
+}
+
+int main()
+{
+  typeText("Wake up, Neo...", 3000);
+  //cout << "\r               \r";
+  typeText("The Matrix has you...", 3000);
+  //cout << "\r                     \r";
+  typeText("Follow the white rabbit.", 3000);
+  //cout << "\r                        \r";
+  cout << "Knock, knock, Neo.\a";
+
+  return 0;
+}
