0979fb9e3a971be35e47f77288ef6f434214bde6
Adam Kuziemski <kuziemski.a@gmail.com>
2016-12-05 16:35:49 +0000

    Update and rename main.cpp to mersenne_twister/example.cpp

--- mersenne_twister/example.cpp
+++ mersenne_twister/example.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 int main()
 {
-  unsigned howmany = 2000;
+  unsigned howmany = 200;
   MersenneTwister mt;
+  
   cout <<"Generating "<< howmany <<" random numbers from 0 to 50:\n";
 
   for(unsigned i = 0; i < howmany; ++i)
   {
     cout << mt() % 50 <<' ';
-
     if(i && i % 20 == 0) cout <<'\n';
   }
 
