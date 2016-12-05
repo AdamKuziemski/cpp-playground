a44e03460040caf46b99a8d55677a1d4dfcb9416
Adam Kuziemski <kuziemski.a@gmail.com>
2016-12-05 15:47:28 +0000

    Update ftwr.cpp

--- ftwr.cpp
+++ ftwr.cpp
@@ -1,34 +1,30 @@
 /**
 *** This is purely a toy.
-*** I mean, you could use it for displaying dialogues in a jRPG.
-*** Or sending packets at random intervals.
-*** Or playing distracting sounds in a game.
-*** Or something.
-*** This comment looks like this on purpose.
-*** It would be funny if metaprogrammed.
-*** Yep. I'm evil.
-*** Anyways, thanks for reading!
+*** I mean, you could use the same idea for displaying dialogues in a jRPG,
+*** or sending packets at random intervals, or playing distracting sounds in a game.
 **/
 
+/** STL **/
 #include <iostream>
 #include <chrono>
 #include <thread>
 #include <random>
 using namespace std;
 
-void typeText(string text, int showFor = 0, bool clear = true)
+void typeText(string text, int showFor = 0, bool clear = false)
 {
-  static mt19937 mt(chrono::system_clock::now().time_since_epoch().count());
+  static mt19937 rnd(chrono::system_clock::now().time_since_epoch().count());
+  using ms = chrono::milliseconds;
 
   for(char c : text)
   {
     cout << c;
     // sleep for random times to achieve the effect of typing
-    this_thread::sleep_for(chrono::milliseconds(40 + (mt() % 460)));
+    this_thread::sleep_for(ms(40 + (rnd() % 460)));
   }
 
   if(showFor > 0)
-    this_thread::sleep_for(chrono::milliseconds(showFor));
+    this_thread::sleep_for(ms(showFor));
 
   if(clear)
     cout <<'\r'<< string(text.size(), ' ') <<'\r';
@@ -36,9 +32,9 @@ void typeText(string text, int showFor = 0, bool clear = true)
 
 int main()
 {
-  typeText("Wake up, Neo...", 3000);
-  typeText("The Matrix has you...", 3000);
-  typeText("Follow the white rabbit.", 3000);
+  typeText("Wake up, Neo...", 3000, true);
+  typeText("The Matrix has you...", 3000, true);
+  typeText("Follow the white rabbit.", 3000, true);
   cout << "Knock, knock, Neo.\a";
 
   return 0;
